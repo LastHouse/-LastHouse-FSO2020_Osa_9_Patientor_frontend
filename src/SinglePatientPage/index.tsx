0c45fa63e8c342e6a447da73ff0e213c632fd86c
LastHouse <54175608+LastHouse@users.noreply.github.com>
2021-02-23 20:26:23 +0200

    9.17 Single Patient Page done

---
+++ src/SinglePatientPage/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+import { apiBaseUrl } from '../constants';
+import { Patient } from '../types';
+import { useLocation } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
+
+const SinglePatientPage: React.FC = () => {
+  const location = useLocation();
+  //console.log(location.state.id);
+  const [patient, setPatient] = React.useState<Patient | null>();
+  React.useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${location.state.id}`
+        );
+        setPatient(patientFromApi);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPatient();
+  }, [location.state.id]);
+
+  if (!patient) return <div>loading...</div>;
+
+  return (
+    <div>
+      {Object.values(patient).map((patient: Patient) => (
+        <div key={patient.id}>
+          <h1>
+            {patient.name}
+            {patient.gender === 'male' && <Icon name="mars" />}
+            {patient.gender === 'female' && <Icon name="venus" />}
+            {patient.gender === 'other' && <Icon name="other gender" />}
+          </h1>
+          <p>ssn: {patient.ssn}</p>
+          <p>occupation: {patient.occupation}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default SinglePatientPage;
